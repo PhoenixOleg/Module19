966898b0d42a721eff9bf5fe39bf5fcc7668ed50
PhoenixOleg <metall_2000@mail.ru>
2023-12-25 01:52:24 +0300

    Добавлен RegistrationView.cs

--- SocialNetwork/Program.cs
+++ SocialNetwork/Program.cs
@@ -18,43 +18,6 @@ namespace SocialNetwork
                                                 //break;
 
 
-                    //case "2":
-                    //    {
-                            var userRegistrationData = new UserRegistrationData();
-
-                            Console.WriteLine("Для создания нового профиля введите ваше имя:");
-                            userRegistrationData.FirstName = Console.ReadLine();
-
-                            Console.Write("Ваша фамилия:");
-                            userRegistrationData.LastName = Console.ReadLine();
-
-                            Console.Write("Пароль:");
-                            userRegistrationData.Password = Console.ReadLine();
-
-                            Console.Write("Почтовый адрес:");
-                            userRegistrationData.Email = Console.ReadLine();
-
-                            try
-                            {
-                                userService.Register(userRegistrationData);
-
-                                Console.ForegroundColor = ConsoleColor.Green;
-                                Console.WriteLine("Ваш профиль успешно создан. Теперь Вы можете войти в систему под своими учетными данными.");
-                                Console.ForegroundColor = ConsoleColor.White;
-                            }
-                            catch (ArgumentNullException)
-                            {
-                                Console.WriteLine("Введите корректное значение.");
-                            }
-                            catch (Exception)
-                            {
-                                Console.WriteLine("Произошла ошибка при регистрации.");
-                            }
-
-                            break;
-                        }
-                }
-            }
 
         }
     }
