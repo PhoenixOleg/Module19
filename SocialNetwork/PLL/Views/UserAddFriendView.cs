2ff4630343211960e19df70501f15b54d005eb6d
PhoenixOleg <metall_2000@mail.ru>
2023-12-27 01:31:30 +0300

    Код класса UserAddFriendView

--- SocialNetwork/PLL/Views/UserAddFriendView.cs
+++ SocialNetwork/PLL/Views/UserAddFriendView.cs
@@ -1,5 +1,7 @@
-﻿using SocialNetwork.BLL.Models;
+﻿using SocialNetwork.BLL.Exceptions;
+using SocialNetwork.BLL.Models;
 using SocialNetwork.BLL.Services;
+using SocialNetwork.PLL.Helpers;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -18,7 +20,27 @@ namespace SocialNetwork.PLL.Views
         }
         public void Show(User user)
         {
+            var addFriendData = new AddFriendData();
 
+            Console.Write("Введите Email пользователя для добавления в друзья");
+            addFriendData.FriendEmail = Console.ReadLine();
+
+            addFriendData.UserId = user.Id;
+
+            try 
+            {
+                //Вызвать либо новые методы userService, либо сделать friendService с наследованием от userService, чтобы не дублировать код поиска
+            }
+
+            catch (UserNotFoundException)
+            {
+                AlertMessage.Show("Пользователь не найден!");
+            }
+
+            catch(Exception ex)
+            {
+                AlertMessage.Show("Произошла ошибка при при добавлении пользователя в друзья!" + Environment.NewLine + ex.Message);
+            }
         }
     }
 }
