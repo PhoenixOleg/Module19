a2fa1c3d7ccf5f9ddea3a12e33b0fe99e601b6bb
PhoenixOleg <metall_2000@mail.ru>
2023-12-27 01:18:27 +0300

    Добавлен класс AddFriendData

---
+++ SocialNetwork/BLL/Models/AddFriendData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialNetwork.BLL.Models
+{
+    /// <summary>
+    /// Класс с моделью данных для добавления в друзья
+    /// 1. Надо знать ID текущего юзера, вошедшего в систему
+    /// 2. Надо знать Email целевого полтзователя для добавления в друзья. По нему определим его ID
+    /// </summary>
+    public class AddFriendData
+    {
+        public int UserId { get; set; }
+        public string FriendEmail { get; set; }
+    }
+}
