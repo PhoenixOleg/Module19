8e4304cc9a03d94ddfb165e8204d674f4b6ab5f4
PhoenixOleg <metall_2000@mail.ru>
2023-12-28 22:06:25 +0300

    Добавлен первый тест на прохождение аутентификации

---
+++ SocialNetwork.Tests/TestClass.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using SocialNetwork.BLL;
+using SocialNetwork.BLL.Models;
+using SocialNetwork.BLL.Services;
+
+namespace SocialNetwork.Tests
+{
+    [TestFixture]
+    public class TestClass
+    {
+        private UserService userService;
+
+        public TestClass() 
+        { 
+            userService = new UserService();
+        }
+
+        [Test]
+        public void AuthenticateMustBeValid()
+        {
+            UserAuthenticationData userAuthenticationData = new()
+            {
+                Email = "first@gmail.com",
+                Password = "12345678"
+            };
+
+            Assert.That (userService.Authenticate(userAuthenticationData) != null);
+        }
+
+    }
+}
